a42b2b363d6dc02ac35bc1a3d6922136645b61e6
kstray <36299019+kstray@users.noreply.github.com>
2020-03-02 13:53:13 +0100

    Add files via upload
    Updated versions

---
+++ task104_plot_update.m
@@ -0,0 +1,65 @@
+task104_template;
+
+data1 = load('task104_update.mat');
+data1 = data1.var;
+
+select = 1:125:length(data1);
+t = data1(1, :);
+t = t(select);
+
+e_measured = data1(2, select);
+e_opt = data1(3, select);
+
+lambda_measured = data1(4, select);
+lambda_opt = data1(5, select);
+
+p_measured = data1(6, select);
+p_opt = data1(7, select);
+
+
+%Constraint
+
+alpha = 0.2;
+beta = 20;
+lambda_t = 2*pi/3;
+e_con = @(lambda_k) alpha*exp(-beta*(lambda_k - lambda_t).^2);
+
+
+%Plot elevation
+figure(1)
+plot(t, e_measured);
+hold on;
+plot(t, e_opt);
+plot(t, rad2deg(e_con(deg2rad(lambda_opt))), '-.k', 'LineWidth', 1);
+legend('e_{measured}', 'e_{optimal}', 'e_{constraint}');
+xlabel('Time [s]');
+ylabel('Elevation [deg]');
+xlim([0, 20]);
+print -depsc task104_e_update
+
+
+%Plot lambda
+figure(2)
+plot(t, lambda_measured);
+hold on;
+plot(t, lambda_opt);
+legend('\lambda_{measured}', '\lambda_{optimal}');
+xlabel('Time [s]');
+ylabel('Travel [deg]');
+xlim([0, 20]);
+print -depsc task104_travel_update
+
+
+%Plot pitch
+figure(3)
+plot(t, p_measured);
+hold on;
+plot(t, p_opt);
+yline(rad2deg((30*pi)/180),'-.k', 'LineWidth', 1);
+yline(-rad2deg((30*pi)/180),'-.k', 'LineWidth', 1);
+legend('p_{measured}', 'p_{optimal}', 'p_{constraint}');
+xlabel('Time [s]');
+ylabel('Pitch [deg]');
+xlim([0, 20]);
+ylim([-35, 35]);
+print -depsc task104_p_update
